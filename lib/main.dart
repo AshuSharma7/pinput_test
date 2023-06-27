2db4992afa67872a2074d6efda92b006e9b8054f
ashusharma70 <ashu7073657271@gmail.com>
2023-06-27 16:31:25 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:example/pinput_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:story_view/story_view.dart';
+
+Future<void> main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: PinPage());
+  }
+}
+
